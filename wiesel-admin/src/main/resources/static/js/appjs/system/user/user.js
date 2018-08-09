4041a2b2cb4a97912bbdb5bd6a042517be55a569
wujian <502393098@qq.com>
2018-08-09 20:27:50 +0800

    用户模块调整，ztree依赖补充

--- wiesel-admin/src/main/resources/static/js/appjs/system/user/user.js
+++ wiesel-admin/src/main/resources/static/js/appjs/system/user/user.js
@@ -1,50 +1,17 @@
 var prefix = "/sys/user"
 $(function() {
-	var deptId = '';
-	getTreeData();
-	load(deptId);
+
+	loadTree();
+	load();
 });
 
-function load(deptId) {
-	$('#exampleTable')
-		.bootstrapTable(
-			{
-				method : 'get', // 服务器数据的请求方式 get or post
+function load() {
+	 app.table('#table',{
 				url : prefix + "/list", // 服务器数据的加载地址
-				// showRefresh : true,
-				// showToggle : true,
-				// showColumns : true,
 				iconSize : 'outline',
-				toolbar : '#exampleToolbar',
-				striped : true, // 设置为true会有隔行变色效果
+				toolbar : '#toolbar',
 				dataType : "json", // 服务器返回的数据类型
-				pagination : true, // 设置为true会在底部显示分页条
-				// queryParamsType : "limit",
-				// //设置为limit则会发送符合RESTFull格式的参数
-				singleSelect : false, // 设置为true将禁止多选
-				// contentType : "application/x-www-form-urlencoded",
-				// //发送到服务器的数据编码类型
-				pageSize : 10, // 如果设置了分页，每页数据条数
-				pageNumber : 1, // 如果设置了分布，首页页码
-				// search : true, // 是否显示搜索框
-				showColumns : false, // 是否显示内容下拉框（选择显示的列）
-				sidePagination : "server", // 设置在哪里进行分页，可选值为"client" 或者
-				// "server"
-				queryParams : function(params) {
-					return {
-						// 说明：传入后台的参数包括offset开始索引，limit步长，sort排序列，order：desc或者,以及所有列的键值对
-						limit : params.limit,
-						offset : params.offset,
-						name : $('#searchName').val(),
-						deptId : deptId
-					};
-				},
-				// //请求服务器数据时，你可以通过重写参数的方式添加一些额外的参数，例如 toolbar 中的参数 如果
-				// queryParamsType = 'limit' ,返回参数必须包含
-				// limit, offset, search, sort, order 否则, 需要包含:
-				// pageSize, pageNumber, searchText, sortName,
-				// sortOrder.
-				// 返回false将会终止请求
+				method : 'get', 
 				columns : [
 					{
 						checkbox : true
@@ -82,35 +49,31 @@ function load(deptId) {
 						field : 'id',
 						align : 'center',
 						formatter : function(value, row, index) {
-							var e = '<a  class="btn btn-primary btn-sm ' + s_edit_h + '" href="#" mce_href="#" title="编辑" onclick="edit(\''
+							var e = '<a  class="btn btn-success btn-sm ' + s_edit_h + '" href="#" mce_href="#" title="编辑" onclick="edit(\''
 								+ row.userId
-								+ '\')"><i class="fa fa-edit "></i></a> ';
-							var d = '<a class="btn btn-warning btn-sm ' + s_remove_h + '" href="#" title="删除"  mce_href="#" onclick="remove(\''
+								+ '\')"><i class="fa fa-edit ">编辑</i></a> ';
+							var d = '<a class="btn btn-danger btn-sm ' + s_delete_h + '" href="#" title="删除"  mce_href="#" onclick="remove(\''
 								+ row.userId
-								+ '\')"><i class="fa fa-remove"></i></a> ';
-							var f = '<a class="btn btn-success btn-sm ' + s_resetPwd_h + '" href="#" title="重置密码"  mce_href="#" onclick="resetPwd(\''
+								+ '\')"><i class="fa fa-remove">删除</i></a> ';
+							var f = '<a class="btn btn-info btn-sm ' + s_resetPwd_h + '" href="#" title="重置密码"  mce_href="#" onclick="resetPwd(\''
 								+ row.userId
-								+ '\')"><i class="fa fa-key"></i></a> ';
+								+ '\')"><i class="fa fa-key">重置密码</i></a> ';
 							return e + d + f;
 						}
-					} ]
+					} ],
+				params:function(){
+			           return $("#queryForm").serializeObject();
+			     }
 			});
 }
 function reLoad() {
-	$('#exampleTable').bootstrapTable('refresh');
+	$('#table').bootstrapTable('refresh');
 }
 function add() {
-	// iframe层
-	layer.open({
-		type : 2,
-		title : '增加用户',
-		maxmin : true,
-		shadeClose : false, // 点击遮罩关闭层
-		area : [ '800px', '520px' ],
-		content : prefix + '/add'
-	});
+	var url=prefix + '/add';
+	app.layer_show({title:'添加用户',content : url});
 }
-function remove(id) {
+function del(id) {
 	layer.confirm('确定要删除选中的记录？', {
 		btn : [ '确定', '取消' ]
 	}, function() {
@@ -132,14 +95,8 @@ function remove(id) {
 	})
 }
 function edit(id) {
-	layer.open({
-		type : 2,
-		title : '用户修改',
-		maxmin : true,
-		shadeClose : false,
-		area : [ '800px', '520px' ],
-		content : prefix + '/edit/' + id // iframe的url
-	});
+	var url=prefix + '/edit/'+id;
+	app.layer_show({title:'用户修改',content : url});
 }
 function resetPwd(id) {
 	layer.open({
@@ -151,56 +108,71 @@ function resetPwd(id) {
 		content : prefix + '/resetPwd/' + id // iframe的url
 	});
 }
-function batchRemove() {
-	var rows = $('#exampleTable').bootstrapTable('getSelections'); // 返回所有选择的行，当没有选择的记录时，返回一个空数组
+function batchDelete() {
+	var rows = $('#table').bootstrapTable('getSelections'); // 返回所有选择的行，当没有选择的记录时，返回一个空数组
 	if (rows.length == 0) {
-		layer.msg("请选择要删除的数据");
+		layer.msg("请选择要删除的数据",{time:600});
 		return;
 	}
-	layer.confirm("确认要删除选中的'" + rows.length + "'条数据吗?", {
-		btn : [ '确定', '取消' ]
-	// 按钮
-	}, function() {
-		var ids = new Array();
-		// 遍历所有选择的行数据，取每条数据对应的ID
-		$.each(rows, function(i, row) {
-			ids[i] = row['userId'];
-		});
-		$.ajax({
-			type : 'POST',
-			data : {
-				"ids" : ids
-			},
-			url : prefix + '/batchRemove',
-			success : function(r) {
-				if (r.code == 0) {
-					layer.msg(r.msg);
-					reLoad();
-				} else {
-					layer.msg(r.msg);
-				}
-			}
-		});
-	}, function() {});
+	
+	app.modalConfirm('确定要删除选中的记录？',
+			function() {
+						var ids = new Array();
+						$.each(rows, function(i, row) {
+							ids[i] = row['userId'];
+						});
+						var data ={"ids":ids};
+						app._ajax(	{url : prefix + "/batchDelete",
+								     data :data
+								})
+	                    }			
+	 );
 }
-function getTreeData() {
+
+
+function loadTree() {
+	var setting = {
+		view : {
+			selectedMulti : false
+		},
+		data : {
+			key : {
+				title : "title"
+			},
+			simpleData : {
+				enable : true,
+				idKey : "id",
+				pIdKey : "pid"
+			}
+		},
+		callback : {
+			onClick : function(event, treeId, treeNode) {
+				tree.expandNode(treeNode);
+				$("#deptId").val(treeNode.id);
+				$("#parentId").val(treeNode.pId);
+				//$.table.search();
+			}
+		}
+	}
 	$.ajax({
 		type : "GET",
-		url : "/system/sysDept/tree",
-		success : function(tree) {
-			loadTree(tree);
+		url : "/sys/dept/tree",
+		success : function(result) {
+			tree = $.fn.zTree.init($("#deptTree"), setting, result); //.expandAll(true);
+			 // 展开第一级节点
+		    var nodes = tree.getNodesByParam("level", 0);
+		    for (var i = 0; i < nodes.length; i++) {
+		        tree.expandNode(nodes[i], true, false, false);
+		    }
+		    // 展开第二级节点
+		    nodes = tree.getNodesByParam("level", 1);
+		    for (var i = 0; i < nodes.length; i++) {
+		        tree.expandNode(nodes[i], true, false, false);
+		    } 
 		}
 	});
-}
-function loadTree(tree) {
-	$('#jstree').jstree({
-		'core' : {
-			'data' : tree
-		},
-		"plugins" : [ "search" ]
-	});
-	$('#jstree').jstree().open_all();
-}
+};
+
 $('#jstree').on("changed.jstree", function(e, data) {
 	if (data.selected == -1) {
 		var opt = {
